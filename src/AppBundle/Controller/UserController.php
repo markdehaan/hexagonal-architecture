96fe7f0b9455902d65103c7393d18f2f91126673
Mark de Haan <mark.dehaan@webstores.nl>
2017-10-23 22:18:43 +0200

    source added

---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\User;
+use AppBundle\Form\UserType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class UserController extends Controller
+{
+    public function listAction()
+    {
+        $users = $this->getDoctrine()->getRepository(User::class)->findAll();
+
+        return $this->render('AppBundle:User:list.html.twig', ['users' => $users]);
+    }
+
+    public function createAction(Request $request)
+    {
+        $form = $this->createForm(UserType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var User $user */
+            $user = $form->getData();
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($user);
+            $manager->flush();
+
+            $message = new \Swift_Message('Welkom');
+            $message->setFrom('no_reply@pizza-avond.webstores');
+            $message->setTo($user->getEmail());
+            $message->setBody(sprintf('Hallo %s, ook zin in een pizza?', $user->getName()));
+            $this->get('mailer')->send($message);
+
+            return $this->redirectToRoute('user_list');
+        }
+
+        return $this->render('AppBundle:User:create.html.twig', ['form' => $form->createView()]);
+    }
+}
